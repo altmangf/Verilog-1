28071561923a60fc7b05840788337b5452dfda48
Gabriel Altman <24990525+altmangf@users.noreply.github.com>
2018-02-28 18:28:07 -0700

    Add files via upload

--- Project1_top.v
+++ Project1_top.v
@@ -7,9 +7,12 @@ module Project1_top(SW, KEY, HEX0, HEX1, LED);
 	output[7:0]HEX1;
 	output[9:0]LED;
 	
+	
 	wire [3:0]X;
 	wire [3:0]Y;
 	wire [7:0]Z;
+	wire [7:0]S;
+	wire Cout;
 	
 	assign HEX0[7] = 1'b0001;
 	assign HEX1[7] = 1'b0001;
@@ -18,8 +21,8 @@ module Project1_top(SW, KEY, HEX0, HEX1, LED);
 	assign Y = SW[7:4];
 	assign Z = SW[7:0];
 	
-	//add Inst1(1'b0, X[3:0], Y[3:0], S, Cout); //LEAVE THIS COMMENTED OUT TO TEST 7 SEGMENT DISPLAY
+	add Inst1(1'b0, X[3:0], Y[3:0], S, Cout); //LEAVE THIS COMMENTED OUT TO TEST 7 SEGMENT DISPLAY
 	//SevenSegment Inst_1(S, HEX0, HEX1);		  //LEAVE THIS COMMENTED OUT TO TEST 7 SEGMENT DISPLAY
-	SevenSegment Inst_1(SW, HEX0, HEX1);		  //DO NOT LEAVE THIS COMMENTED OUT TO TEST 7 SEGMENT DISPLAY
-	//SevenSegment Inst_1(S, HEX0, HEX1);       //USE THIS INSTANTIATION FOR TESTING THE ADDER (DOES NOT WORK YET)
+	//SevenSegment Inst_1(SW, HEX0, HEX1);		  //DO NOT LEAVE THIS COMMENTED OUT TO TEST 7 SEGMENT DISPLAY
+	SevenSegment Inst_1(S, HEX0, HEX1);       //USE THIS INSTANTIATION FOR TESTING THE ADDER (DOES NOT WORK YET)
 endmodule
