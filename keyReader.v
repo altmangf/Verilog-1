81df22a80f771cbe06d292e5ff7b06acf32346f8
Gabriel Altman <24990525+altmangf@users.noreply.github.com>
2018-03-01 21:57:48 -0700

    Add files via upload

---
+++ keyReader.v
@@ -0,0 +1,47 @@
+module keyReader (key, mode);
+
+input [0:1]key;
+output reg [1:0]mode;
+
+
+always @(posedge key[0]) 
+		begin
+		mode[0] = ~mode[0];
+		end
+always @(posedge key[1])
+		begin
+		mode[1] = ~mode[1];
+		end
+		
+endmodule
+
+
+
+//CODE BELOW THIS LINE DOES NOT WORK
+//_______________________________________
+/*
+always @(posedge key) 
+		begin
+		case (key[0])  	
+			1'b1: mode[0] = ~mode[0];
+		endcase	
+		case (key[1])
+			1'b1: mode[1] = ~mode[1];
+		endcase
+		end
+		
+endmodule
+		
+
+	always @(posedge key) 
+		begin
+		case (key[1:0])  	
+			2'b00: mode=2'b00;
+			2'b01: mode=2'b01;
+			2'b10: mode=2'b10;
+			2'b11: mode=2'b11;
+		endcase
+		end
+		
+endmodule	
+*/
---
+++ multiplexer2_1.v
@@ -0,0 +1,16 @@
+module multiplexer2_1 (SELECT, A, B, OUT);	
+ 
+ input [1:0]SELECT; 	// 1-bit control signal (MODE) 
+ input A;
+ input B;
+
+ output reg OUT;				// The output of the MUX 
+ 
+ always @(SELECT) 
+	begin
+		if (SELECT == 2'b00) 
+			OUT = A;
+		else if (SELECT == 2'b01)
+			OUT = B;
+	end
+ endmodule 
