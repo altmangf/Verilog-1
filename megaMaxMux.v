6da9ed757e5203c91cac4a92c5b2a70f2b6fc697
Gabriel Altman <24990525+altmangf@users.noreply.github.com>
2018-03-03 15:59:19 -0700

    Add files via upload

--- megaMaxMux.v
+++ megaMaxMux.v
@@ -30,7 +30,7 @@ module megaMaxMux(x, y, maxValueBoolean);
 	assign maxMux0  = y[3]+y[2]+y[1]+y[0];
 	assign maxMux1  = y[3]+y[2]+y[1]+(y[0]&(y[3]+y[2]+y[1]));
 	assign maxMux2  = y[3]+y[2]+(y[1]&y[0]);
-	assign maxMux3  = y[3]+y[2]+((y[1]&y[0])+(y[3]+y[2]));
+	assign maxMux3  = y[3]+y[2];
 	assign maxMux4  = y[3]+(y[2]&(y[1]+y[0]));
 	assign maxMux5  = y[3]+(y[2]&y[1]);
 	assign maxMux6  = y[3]+(y[2]+y[1]+y[0]);
