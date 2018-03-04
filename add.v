60e321a7e2126c958d782ba84277f31393079e61
Gabriel Altman <24990525+altmangf@users.noreply.github.com>
2018-03-04 14:17:21 -0700

    Add files via upload

--- add.v
+++ add.v
@@ -4,18 +4,18 @@ ECEN 2350 Digital Logic
 March, 2018
 */
 
-module add(carryin, Xin, Yin, S, carryout);
-	parameter n=4;
+module add#(parameter n=8)(carryin, Xin, Yin, S, carryout);
+	
 	input 	carryin;
 	input 	[n-1:0]Xin;
 	input		[n-1:0]Yin;
-	output 	[n-1:0]S;
+	output 	[7:0]S;
 	output carryout;
-	wire [n+1:0]C;//[n:0]C;
+	wire [n:0]C;
 	
 	genvar i;
 	assign C[0]=carryin;
-	assign carryout = C[n+1];	//was C[n];
+	assign carryout = C[n];	
 	
 	generate
 		for(i = 0;i <= n-1;i=i+1)
